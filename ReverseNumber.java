c2f6acbf97d3baccebc3360500ea8921e798d53c
Inamul <inamulhoque097@gmail.com>
2022-05-30 21:57:19 +0530

    Create: Reverse a number

---
+++ ReverseNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void reverse(int num){
+        int rev = 0;
+        while (num!=0){
+            int rem = num % 10;
+            rev = rev *10 +rem;
+            num = num/10;
+        }
+        System.out.println(rev);
+    }
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = myObj.nextInt();
+        reverse(num);
+    }
+}
